90eeeefc029e2245d885d5ba472922e7b80d92d1
julian-saez <juliansaez.projects@gmail.com>
2020-11-08 21:00:05 -0300

    agregué el dark mode

---
+++ dark-mode.js
@@ -0,0 +1,16 @@
+let darkBtn = document.getElementById("mode-btn");
+
+darkBtn.addEventListener("click", function mode(){
+    let background = document.getElementById("fondo")
+    let backgroundTrending = document.getElementById("trending-gifos")
+
+    if(background.className == "light" && backgroundTrending.className == "light-trending"){
+        background.className = "dark";
+        backgroundTrending.className = "dark-trending";
+    }else{
+        background.className = "light";
+        backgroundTrending.className = "light-trending";
+    }
+})
+
+
