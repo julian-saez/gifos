e34bd2aef90f580a5330de0d5dd06b486b50d388
julian-saez <juliansaez.projects@gmail.com>
2020-11-15 21:15:51 -0300

    va queriendo el fetch

--- home - gifos/app.js
+++ home - gifos/app.js
@@ -1,67 +1,58 @@
+const urlTrendings = "http://api.giphy.com/v1/gifs/trending?api_key=3573pz5lsjTE2QvU9Ii5g3t7Ky3svfUm&limit=3";
+
 /**
  * CREO LOS CONTENEDORES DONDE ESTARÁN LOS GIFS
  */
 
+var gifBox;
+var enlace; 
+var idNumber = 0;
 
 for(let i = 0; i <= 2; ++i){
-    var gifBox = document.createElement("img")
-    var enlace = document.createElement("a")
+    gifBox = document.createElement("img")
+    enlace = document.createElement("a")
     gifBox.appendChild(enlace)
+    gifBox.id = `gfnumber${idNumber}`;
 
     let container = document.getElementById("list-gifs")
     container.appendChild(gifBox)
+    idNumber = idNumber + 1; 
+}
+
+/**
+ * FUNCIÓN PARA RECARGAR Y CREAR MÁS GIFS
+ */
+
+const reload = function (x) {
+    for(let i = 0; i <= 9; ++i){
+        gifBox = document.createElement("img")
+        enlace = document.createElement("a")
+        gifBox.appendChild(enlace)
+        gifBox.id = `gfnumber${idNumber}`;
+    
+        let container = document.getElementById("list-gifs")
+        container.appendChild(gifBox)
+    }
 }
 
 
 /**
- * COMUNICACIÓN CON LAS API'S
+ * OBTENCIÓN DE LOS DATOS DE LA API
  */
 
-const url = "http://giphy.bintray.com/giphy-sdk";
 
-// Función para plasmar los gifs
-
-const getGif = (image) => {
-    gifBox.getAttribute('src', image)
-}
+ const overWrite = function (x) {
+     gifBox.src = x;
+ }  
 
 
-// fetch(url)
-// .then(response => response.json())
-// .then(binaryLargeObject => {
-//     const domString = URL.createObjectURL(binaryLargeObject);
-//     getGif(domString)
-// })
+ fetch(urlTrendings)
+ .then(res => res.json() )
+ .then(res => {
+     console.log(res)
+     overWrite(res.data[0].images.original.url)
+ })
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    debugger
-    getGif(data.image)
-})
-
-/**  ---   LLAMO LOS BOTONES DEL HTML   ---  **/
-
-
-// let btnBack = document.getElementById("back-btn")
-// let btnNext = document.getElementById("next-btn")
-
-// // btnBack.addEventListener("click", show)
-// btnNext.addEventListener("click", show)
-
-
-
-// const show = () => {
-//     for(let i = 0; i <= 2; ++i){
-
-//         var gifBox = document.createElement("img")
-//         var enlace = document.createElement("a")
-    
-//         gifBox.appendChild(enlace)
-    
-//         gifBox.src = arrayTest[i];
-    
-//         let container = document.getElementById("list-gifs")
-//         container.appendChild(gifBox)
-//     }
-// }
+ .catch(error => {
+     console.log("Error n°", error)
+ })
