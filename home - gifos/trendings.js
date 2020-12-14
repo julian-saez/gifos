fd3e7de32fe742d71c4b379d923c3e12297de065
julian-saez <juliansaez.projects@gmail.com>
2020-12-14 04:07:03 -0300

    ya esta funcionando el boton back en trending

--- home - gifos/trendings.js
+++ home - gifos/trendings.js
@@ -6,7 +6,7 @@ const urlTrending = "http://api.giphy.com/v1/gifs/trending?api_key=3573pz5lsjTE2
 let arrayTrendings = [];
 
 function getTrendings(){
-    fetch(`${urlTrending}&limit=15`)
+    fetch(`${urlTrending}&limit=15&offset=15`)
     .then(res => res.json() )
     .then(res => {
         // Recorro los objetos del request
@@ -15,6 +15,7 @@ function getTrendings(){
 
         // Pusheo los objetos al array "arrayTrendings"
         arrayTrendings.push(valores);
+        console.log(arrayTrendings)
     })
     printTrendings()
     })
@@ -29,9 +30,7 @@ getTrendings()
 let container = document.getElementById("list-gifs")
 
 const resize = () => {
-    if(innerWidth > 768) {
-        btnVisibility()
-    }else{
+    if(innerWidth < 768) {
         btnMobileNext()
     }
 }
@@ -41,29 +40,34 @@ let idTrend = 0;
 
 // Función para cargar los gifs trendings
 function printTrendings(){
-    for(let i = 0; i <= 2; ++i){
-        // Creo los elementos
-        let gifBox = document.createElement("img")
-        let title = document.createElement("h2")
-        let username = document.createElement("h3")
-
-        // Declaro los hijos de la caja de los gifs
-        gifBox.appendChild(title)
-        gifBox.appendChild(username)
-
-        // Le coloco los idTren a cada caja y les agrego el titulo y la url del gif
-        gifBox.idTrend = `trend${idTrend}`
-        title.innerHTML = arrayTrendings[idTrend].title
-        username.innerHTML = `Creator: ${arrayTrendings[idTrend].username}`
-        gifBox.src = arrayTrendings[idTrend].images.original.url
-
-        // Declaro a las boxes como hijo de container 
-
-        container.appendChild(gifBox)
-
-        // Aumento el valor de idTrend para luego iterar nuevamente sobre los idTrend de las boxes
-        idTrend = idTrend + 1;
+    if(container.childElementCount == 0){
+        for(let i = 0; i <= 2; ++i){
+            // Creo los elementos
+            let gifBox = document.createElement("img")
+            let title = document.createElement("h2")
+            let username = document.createElement("h3")
+    
+            // Declaro los hijos de la caja de los gifs
+            gifBox.appendChild(title)
+            gifBox.appendChild(username)
+    
+            // Le coloco los idTren a cada caja y les agrego el titulo y la url del gif
+            gifBox.idTrend = `trend${idTrend}`
+            title.innerHTML = arrayTrendings[idTrend].title
+            username.innerHTML = `Creator: ${arrayTrendings[idTrend].username}`
+            gifBox.src = arrayTrendings[idTrend].images.preview_webp.url
+    
+            // Declaro a las boxes como hijo de container 
+    
+            container.appendChild(gifBox)
+    
+            // Aumento el valor de idTrend para luego iterar nuevamente sobre los idTrend de las boxes
+            idTrend = idTrend + 1;
+        }
+    }else(container.childElementCount == 2);{
+        console.log("Esta todo OK")
     }
+
     if(idTrend >= 3){
         resize()
     }
@@ -73,36 +77,24 @@ let iOne = 0;
 let iTwo = 1;
 let iThree = 2;
 
+let restOne = 3;
+
+
+let requestCount = 10;
+
 let btnNextG = document.getElementById("btn-next")
-
-
-// Función para mostrar los botones de back y next en DESKTOP
-const btnVisibility = () => {
-    let btnBack = document.createElement("button")
-    let btnBackIMG = document.createElement("img")
-    btnBackIMG.src = 'assets/button-slider-left.svg'
-    btnBack.id = 'btn-back-trendings'
-
-    btnNext = document.createElement("button")
-    let btnNextIMG = document.createElement("img")
-    btnNextIMG.src = 'assets/Button-Slider-right.svg'
-    btnNext.id = 'btn-more-trendings'
-
-
-    // Posiciono los botones de atras y siguiente
-    // container.insertBefore(btnBack, container.children[0])
-    container.insertAdjacentElement('beforebegin',btnBack)
-    container.insertAdjacentElement('afterend',btnNext)
-
-    btnBack.appendChild(btnBackIMG)
-    btnNext.appendChild(btnNextIMG)
-}
+let btnBackG = document.getElementById("btn-back")
 
 btnNextG.addEventListener("click", () => {
+    restOne -= 3
     iOne += 3;
     iTwo += 3;
     iThree += 3;
     function overwritten() {
+        if(iThree >= requestCount){
+            getTrendings()
+            requestCount += 15;
+        }
         container.children[0].setAttribute('src', '')
         container.children[1].setAttribute('src', '')
         container.children[2].setAttribute('src', '')
@@ -116,6 +108,21 @@ btnNextG.addEventListener("click", () => {
         img3.src = arrayTrendings[iThree].images.preview_webp.url
     }
     overwritten()
+})
+
+btnBackG.addEventListener("click", () => {
+    container.children[0].setAttribute('src', '')
+    container.children[1].setAttribute('src', '')
+    container.children[2].setAttribute('src', '')
+
+    let img1 = container.children[0]
+    let img2 = container.children[1]
+    let img3 = container.children[2]
+
+    img1.src = arrayTrendings[iOne - restOne].images.preview_webp.url
+    img2.src = arrayTrendings[iTwo - restOne].images.preview_webp.url
+    img3.src = arrayTrendings[iThree - restOne].images.preview_webp.url
+    restOne += 3
 })
 
 
